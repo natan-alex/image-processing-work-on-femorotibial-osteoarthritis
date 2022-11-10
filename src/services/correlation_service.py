283310245bae192470a01e46df258fec3c803dfe
natan-alex <natancarmo321@gmail.com>
2022-11-10 11:12:39 -0300

    surround find correlation method in a try catch block

--- src/services/correlation_service.py
+++ src/services/correlation_service.py
@@ -10,20 +10,23 @@ class CorrelationService:
         image: Image,
         template: Image,
     ) -> Tuple[Tuple[int, int], Tuple[int, int]]:
-        image_as_np_array = np.array(image)
-        template_as_np_array = np.array(template)
+        try:
+            image_as_np_array = np.array(image)
+            template_as_np_array = np.array(template)
 
-        correlation = cv2.matchTemplate(
-            image_as_np_array,
-            template_as_np_array,
-            cv2.TM_CCOEFF_NORMED
-        )
+            correlation = cv2.matchTemplate(
+                image_as_np_array,
+                template_as_np_array,
+                cv2.TM_CCOEFF_NORMED
+            )
 
-        template_height, template_width, _ = template_as_np_array.shape
-        argmax = np.argmax(correlation)
-        location_y, location_x = np.unravel_index(argmax, correlation.shape)
+            template_height, template_width, _ = template_as_np_array.shape
+            y, x = np.unravel_index(np.argmax(correlation), correlation.shape)
 
-        return (
-            (location_x, location_y),
-            (location_x + template_width, location_y + template_height)
-        )
+            return (
+                (x, y),
+                (x + template_width, y + template_height)
+            )
+        except Exception as e:
+            print(e)
+            return None
