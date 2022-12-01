2c92e38590f3f4e9c2a157ba5ebff4367310fbc2
natan-alex <natancarmo321@gmail.com>
2022-12-01 20:24:20 -0300

    correct build model in model service with the minimal

--- src/services/model_service.py
+++ src/services/model_service.py
@@ -13,31 +13,33 @@ class ModelService:
     @staticmethod
     def build_model(infos: aliases.Datasets):
         try:
-            inception_model = tf.keras.applications.InceptionV3(weights="imagenet", include_top=False)
+            input_layer = tf.keras.Input(shape=configs.MODEL_IMAGES_SIZE + (3,))
+
+            inception_model = tf.keras.applications.InceptionV3(
+                weights="imagenet", include_top=False, input_tensor=input_layer)
 
             for layer in inception_model.layers:
                 layer.trainable = False
 
-            input_layer = tf.keras.Input(shape=configs.MODEL_IMAGES_SIZE + (3,))
+            x = tf.keras.layers.Flatten()(inception_model.output)
 
-            temp = tf.keras.layers.Conv2D(128, (7, 7), padding="same", activation="relu", strides=(2, 2))(input_layer)
-
-            temp = inception_model(temp)
-
-            temp = tf.keras.layers.Dropout(rate=0.2)(temp)
+            x = tf.keras.layers.Dropout(rate=0.2)(x)
 
             output_layer = tf.keras.layers.Dense(
                 len(list(ModelClasses)),
-                activation="softmax")(temp)
+                activation=tf.keras.activations.softmax)(x)
 
-            model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
+            model = tf.keras.Model(inputs=[input_layer], outputs=[output_layer])
+
             model.compile(
                 optimizer=tf.keras.optimizers.Adam(),
-                loss=tf.keras.losses.CategoricalCrossentropy,
-                metrics=[tf.keras.metrics.Accuracy])
+                loss=tf.keras.losses.CategoricalCrossentropy(),
+                metrics=[tf.keras.metrics.Accuracy()])
+
             model.fit_generator(
                 infos[ExpectedSubfolders.TRAIN],
                 epochs=2)
+
             model.summary()
         except Exception as e:
             print(f"Exception on ModelService.load_model_from_file: {e}")
